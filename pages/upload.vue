0e23d6bd66169ddbbc75c08db77115eab47f249d
ayahamaguri <neko.neco.0523@gmail.com>
2021-10-24 11:40:02 +0000

    uploadページ作成

---
+++ pages/upload.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-form>
+      <v-container
+        ><v-row class="align-center"
+          ><v-col cols="4">
+            <v-subheader
+              >YouTubeの動画ID<br />
+              （URLの%以降の文字列）を入力</v-subheader
+            ></v-col
+          >
+          <v-col cols="6"> <v-text-field clearable></v-text-field></v-col
+          ><v-btn color="orange">投稿</v-btn></v-row
+        ></v-container
+      >
+    </v-form>
+  </div>
+</template>
